48a6bffeed470186ee0486a21188714e98138cfc
starlabelgh <hello@starlabel.co>
2022-05-17 23:00:04 +0000

    Initial Commit

---
+++ Quote/QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteApp.swift
+//  Quote
+//
+//  Created by Moses Samanhyia on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
